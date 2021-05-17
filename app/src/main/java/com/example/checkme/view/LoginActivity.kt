1cfcd64c19618dc453abc1202f332c17f2c2f39b
MyeongSeong <ms1994s@naver.com>
2021-05-17 18:12:47 +0900

    Login이후 LoginActivity 스택 제거

--- app/src/main/java/com/example/checkme/view/LoginActivity.kt
+++ app/src/main/java/com/example/checkme/view/LoginActivity.kt
@@ -6,14 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import com.example.checkme.R
 
-class LoginActivity : AppCompatActivity(){
-    private val appCompatButton_Login : AppCompatButton by lazy { findViewById(R.id.appCompatButton_Login) }
+class LoginActivity : AppCompatActivity() {
+    private val appCompatButton_Login: AppCompatButton by lazy { findViewById(R.id.appCompatButton_Login) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         appCompatButton_Login.setOnClickListener {
-            startActivity(Intent(this,MainActivity::class.java))
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP //액티비티 스택제거
+            startActivity(intent)
         }
 
     }
