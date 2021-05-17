f0917cb551a9d07fa3980fc4c5935a16525bd76c
MyeongSeong <ms1994s@naver.com>
2021-05-17 18:00:07 +0900

    커스텀버튼 & homeFragment 메뉴 추가

--- app/src/main/java/com/example/checkme/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/checkme/fragment/HomeFragment.kt
@@ -1,7 +1,20 @@
 package com.example.checkme.fragment
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.example.checkme.R
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button : AppCompatButton by lazy { view.findViewById(R.id.button) }
+        button.setOnClickListener {
+            Toast.makeText(context,"눌려짐",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
